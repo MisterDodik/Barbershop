2f152c6bb3d9cb42054382e24ce39d28c605fbb7
MisterDodik <drazenpetrovic56@gmail.com>
2025-06-16 23:03:21 +0200

    Create api basic and starting endpoints

---
+++ cmd/api/api.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/MisterDodik/Barbershop/internal/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+	store  store.Storage
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
+
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.getHealthHandler)
+
+		r.Route("/appointment", func(r chi.Router) {
+			r.Post("/get_available_dates", app.getAvailableDates) //prilikom loadanja sajta uzeti da je selectedday = null, a to ce automatski biti danasnji dan
+			r.Post("/book/{slotID}", app.bookAppointment)
+		})
+
+		r.Route("/authentication", func(r chi.Router) {
+			r.Post("/user", app.registerUserHandler)
+			// r.Post("/token", app.createTokenHandler)
+		})
+		// r.With(paginate).Get("/", listArticles)                           // GET /articles
+		// r.Get("/search", searchArticles)                                  // GET /articles/search
+		// // Subrouters:
+		// r.Route("/{articleID}", func(r chi.Router) {
+		// 	//r.Use(ArticleCtx)
+		// 	//r.Get("/", getArticle)       // GET /articles/123
+		// })
+	})
+
+	return r
+}
+
+func (app *application) run(mux http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server started at %s", app.config.addr)
+	return srv.ListenAndServe()
+}
