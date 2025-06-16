a920ddd55013e112a41325f5062528a13b6599c6
MisterDodik <drazenpetrovic56@gmail.com>
2025-06-16 23:57:34 +0200

    get from context

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/MisterDodik/Barbershop/internal/store"
+)
+
+type userKey string
+
+const userCtx userKey = "user"
+
+func getUserFromContext(r *http.Request) *store.User {
+	return r.Context().Value(userCtx).(*store.User)
+}
