cfac2ba98c453dbd1d9a86da02751b4dcb45f1b4
MisterDodik <drazenpetrovic56@gmail.com>
2025-06-16 23:07:12 +0200

    Create storage.go

---
+++ internal/store/storage.go
@@ -0,0 +1,32 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+)
+
+var (
+	Error_NotFound       = errors.New("record not found")
+	Error_Conflict       = errors.New("resource already exists")
+	QueryTimeoutDuration = time.Second * 5
+)
+
+type Storage struct {
+	Users interface {
+		Create(context.Context, *User) error
+		GetByID(context.Context, int64) User
+	}
+	TimeSlots interface {
+		GetFreeSlots(context.Context, time.Time) ([]TimeSlot, error)
+		Book(context.Context, int64) error
+	}
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return Storage{
+		Users:     &UserStorage{db},
+		TimeSlots: &TimeSlotsStorage{db},
+	}
+}
