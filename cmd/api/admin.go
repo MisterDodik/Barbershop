50965794bec5639a7100b70138a33054076874aa
MisterDodik <drazenpetrovic56@gmail.com>
2025-06-26 10:49:15 +0200

    Add worker work settings handler

--- cmd/api/admin.go
+++ cmd/api/admin.go
@@ -83,3 +83,69 @@ func (app *application) getBookedDates(w http.ResponseWriter, r *http.Request) {
 		app.internalServerError(w, r, err)
 	}
 }
+
+type WorkSettingsPayload struct {
+	WorkingHours        map[string]string `json:"working_hours" validate:"required,dive,keys,required,endkeys"`
+	AppointmentDuration int               `json:"appointment_duration" validate:"required"`
+	PauseBetween        int               `json:"pause_between" validate:"required"`
+}
+
+func (app *application) updateWorkSettings(w http.ResponseWriter, r *http.Request) {
+	var payload WorkSettingsPayload
+
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	worker := getUserFromContext(r)
+	if worker == nil || worker.Role != "worker" {
+		app.unauthorizedErrorResponse(w, r, fmt.Errorf("you dont have permissions to access this"))
+		return
+	}
+	workerID := worker.ID
+
+	if err := app.store.Workers.CreateOrUpdateSettings(r.Context(),
+		workerID,
+		payload.WorkingHours,
+		payload.AppointmentDuration,
+		payload.PauseBetween); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := app.jsonResponse(w, http.StatusCreated, "successfully updated worker profile"); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+func (app *application) getWorkSettings(w http.ResponseWriter, r *http.Request) {
+	worker := getUserFromContext(r)
+	if worker == nil || worker.Role != "worker" {
+		app.unauthorizedErrorResponse(w, r, fmt.Errorf("you dont have permissions to access this"))
+		return
+	}
+	workerID := worker.ID
+
+	settings, err := app.store.Workers.GetSettings(r.Context(), workerID)
+	if err != nil {
+		switch err {
+		case store.Error_NotFound:
+			app.notFoundResponse(w, r, err)
+			return
+		default:
+			app.internalServerError(w, r, err)
+			return
+		}
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, settings); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
