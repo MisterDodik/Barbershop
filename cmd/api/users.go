12045387b955e00578a6712851b37457b32a020f
MisterDodik <drazenpetrovic56@gmail.com>
2025-07-07 19:37:52 +0200

    Add user info and password reset handlers

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/MisterDodik/Barbershop/internal/store"
@@ -12,6 +13,58 @@ const userCtx userKey = "user"
 
 func getUserFromContext(r *http.Request) *store.User {
 	return r.Context().Value(userCtx).(*store.User)
+}
+
+func (app *application) getMyInfo(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromContext(r)
+
+	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+type ResetPasswordPayload struct {
+	NewPassword string `json:"new_password" validate:"required,min=3,max=72"`
+}
+
+func (app *application) resetPassword(w http.ResponseWriter, r *http.Request) {
+	var payload ResetPasswordPayload
+
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+	user := getUserFromContext(r)
+
+	if err := user.Password.Set(payload.NewPassword); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	err := app.store.Users.ResetPassword(ctx, user)
+	if err != nil {
+		switch err {
+		case store.Error_NotFound:
+			app.notFoundResponse(w, r, fmt.Errorf("no changes were made"))
+			return
+		default:
+			app.internalServerError(w, r, err)
+			return
+		}
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, "password updated"); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
 }
 
 func (app *application) getMyAppointments(w http.ResponseWriter, r *http.Request) {
